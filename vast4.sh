2ea709cf65fde6d2a6490ed04040df13b62ea824
Rhemi7 <54381642+Rhemi7@users.noreply.github.com>
2023-10-14 13:10:24 +0100

    Update vast4.sh

--- vast4.sh
+++ vast4.sh
@@ -45,7 +45,7 @@ sleep 5
 echo "---------------------------"
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x75B612b69D7FAdCF3BD27e2C2C9E938F0f72Fbb9/g' config.conf
 sleep 5
 echo "---------------------------"
 
